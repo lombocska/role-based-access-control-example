b3d21ca5ec899b2e85993b66e923bcade9849f9e
lombosmonika <lombos.monika@gmail.com>
2020-06-23 10:00:52 +0200

    chore: containerization - [X] app layered docker image build - [X] dependencies added as external docker images - [X] integration test got own docker compose

--- src/test/java/io/lombocska/app/RoleBasedAccessControlExampleApplicationIT.java
+++ src/test/java/io/lombocska/app/RoleBasedAccessControlExampleApplicationIT.java
@@ -2,9 +2,11 @@ package io.lombocska.app;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-class RoleBasedAccessControlExamlpleApplicationTests {
+@ActiveProfiles("it")
+class RoleBasedAccessControlExampleApplicationIT {
 
 	@Test
 	void contextLoads() {
